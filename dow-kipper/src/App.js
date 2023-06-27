a3c1621eb7f13a46c742c361af23f381e35ca937
Chen <xchendd214@gmail.com>
2023-06-27 15:25:03 -0400

    added login and signup pages

--- dow-kipper/src/App.js
+++ dow-kipper/src/App.js
@@ -1,10 +1,14 @@
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 import "./index.css";
 
 function App() {
   return (
     <div className="App">
-        <Home />
+        {/* <Home /> */}
+        <Signup />
+        {/* <Login /> */}
     </div>
   );
 }
