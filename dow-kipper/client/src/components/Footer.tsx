abae932dfd1f81a723c2201d6492fa046a60a9d8
Ian Ouyoung <ianouyoung@gmail.com>
2023-06-28 15:30:23 -0400

    Standardized Development File Structure

--- dow-kipper/client/src/components/Footer.tsx
+++ dow-kipper/client/src/components/Footer.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <footer className='my-8'>
+      <footer className="my-8">
         <div className="cards grid grid-cols-3 gap-4">
           <div className="card p-4">
-            <h3>About:</h3>
+            <h3 className="font-semibold">About:</h3>
             <p className="">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Cupiditate id voluptas ab perferendis laborum. Alias ut nulla
@@ -15,7 +15,7 @@ const Footer = () => {
             </p>
           </div>
           <div className="card p-4">
-            <h3>Join Our Community:</h3>
+            <h3 className="font-semibold">Join Our Community:</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
               velit dolorum voluptate aut alias sunt fuga, odit reiciendis odio
@@ -24,7 +24,7 @@ const Footer = () => {
             </p>
           </div>
           <div className="card p-4">
-            <h3>Partner with Us:</h3>
+            <h3 className="font-semibold">Partner with Us:</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Et esse
               illo similique, odit placeat dignissimos atque accusantium
---
+++ dow-kipper/server/src/main/java/data/CollectionJdbcTemplateRepository.java
@@ -0,0 +1,60 @@
+package data;
+
+import data.mappers.CollectionMapper;
+import models.Collection;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Repository
+public class CollectionJdbcTemplateRepository implements CollectionRepository {
+    private final JdbcTemplate jdbcTemplate;
+    private final UserRepository userRepository;
+
+    // The UserRepository should be injected into the constructor
+    // along with the JdbcTemplate.
+    public CollectionJdbcTemplateRepository(JdbcTemplate jdbcTemplate, UserRepository userRepository) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<Collection> findAllByUserId(int userId) {
+        final String sql = "select collection_id, user_id, name from collection where user_id = ?;";
+        return jdbcTemplate.query(sql, new CollectionMapper(userRepository), userId);
+    }
+
+    @Override
+    public Collection findById(long collectionId) {
+        final String sql = "select collection_id, user_id, name from collection where collection_id = ?;";
+        Collection result = jdbcTemplate.query(sql, new CollectionMapper(userRepository), collectionId)
+                .stream()
+                .findAny().orElse(null);
+        return result;
+    }
+
+    @Override
+    @Transactional
+    public int addCollection(Collection collection) {
+        final String sql = "INSERT INTO collection (user_id, name) VALUES (?, ?)";
+
+        return jdbcTemplate.update(sql, collection.getUser().getId(), collection.getName());
+    }
+
+    @Override
+    public boolean updateCollection(Collection collection) {
+        final String sql = "update collection set "
+                + "user_id = ?, "
+                + "name = ? "
+                + "where collection_id = ?";
+
+        return jdbcTemplate.update(sql, collection.getUser().getId(), collection.getName(), collection.getId()) > 0;
+    }
+
+    @Override
+    public boolean deleteCollection(int collectionId) {
+        final String sql = "delete from collection where collection_id = ?";
+        return jdbcTemplate.update(sql, collectionId) > 0;
+    }
+}
