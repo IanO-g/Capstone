bf582c73a810cc75a1261535f20f1a853a64a6c5
Chen <xchendd214@gmail.com>
2023-07-03 15:26:33 -0400

    refactored TSX code

---
+++ dow-kipper-typescript/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Collections from "./components/Collections";
+import Formsubmit from "./components/Formsubmit";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      {/* <Navbar /> */}
+      {/* <Collections /> */}
+      {/* <Formsubmit /> */}
+    </div>
+  );
+};
+
+export default App;
