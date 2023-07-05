292b0c61312daae32171d74b292281d1ca60780f
Ian Ouyoung <ianouyoung@gmail.com>
2023-07-05 16:53:08 -0400

    environemnt variable for token and api service

--- dow-kipper/client/src/services/PriceApi.tsx
+++ dow-kipper/client/src/services/PriceApi.tsx
@@ -1,27 +1,28 @@
-const apiToken = process.env.REACT_APP_API_TOKEN;
+import fetch from 'node-fetch';
 
-import('node-fetch').then(({default: fetch}) => {
-    fetch(`https://www.pricecharting.com/api/offers?t=${apiToken}&id=630417`)
-    .then(response => response.json())
-    .then(data => {
-        console.log('Status: ', data.status);
-        data.offers.forEach((offer, index) => {
-            console.log('Offer ' + (index + 1) + ': ', JSON.stringify(offer, null, 2));
-        });
-    })
-    .catch(error => console.error('Error:', error));
-});
+//API URLs
+const priceChartingApiProductUrl = 'https://www.pricecharting.com/api/product';
+const priceChartingApiProductsUrl = 'https://www.pricecharting.com/api/products';
+const priceChartingOfferApiUrl = 'https://www.pricecharting.com/api/offers';
 
-// to get sold history
+//API key
+const apiToken = process.env.REACT_APP_API_TOKEN;;
 
-import('node-fetch').then(({default: fetch}) => {
-    fetch(`https://www.pricecharting.com/api/offers?t=${apiToken}&id=630417&status=sold`)
-    .then(response => response.json())
-    .then(data => {
-        console.log('Status: ', data.status);
-        data.offers.forEach((offer, index) => {
-            console.log('Offer ' + (index + 1) + ': ', JSON.stringify(offer, null, 2));
-        });
-    })
-    .catch(error => console.error('Error:', error));
-});
+export async function fetchItemById(itemId: string): Promise<any> {
+  const response = await fetch(`${priceChartingApiProductUrl}?t=${apiToken}&id=${itemId}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function fetchProductsByKeyword(keyword: string): Promise<any> {
+  const response = await fetch(`${priceChartingApiProductsUrl}?t=${apiToken}&q=${keyword}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function fetchSoldItems(itemId: string): Promise<any> {
+  const response = await fetch(`${priceChartingOfferApiUrl}?t=${apiToken}&id=${itemId}&status=sold`);
+  const data = await response.json();
+  return data;
+}
+
